2ae9c0407e03f63b8542ac30dbb724dde5e33b78
DidierVanspauwen <113337439+DidierVanspauwen@users.noreply.github.com>
2023-03-15 09:45:57 +0100

    index

--- index.php
+++ index.php
@@ -11,12 +11,19 @@
     <h1>Welkom</h1>
 
     <!--greyCircle-->
-    <div id="circle1">Profiel aanmaken</div>
+    <div id="circle1">
+        <div id="makePr"><a href=newProfile.php >Profiel aanmaken</a></div>
+    </div>
 
     <!--redCircle-->
-    <div id="circle2">Profiel bekijken</div><br>
+    <div id="circle2">
+        <div id="viewPr"><a href=viewProfile.php >Profiel bekijken</div>
+    </div><br>
 
     <!--blueCircle-->
-    <div id="circle3">home</div>
+    <div id="circle3">
+        <div id="home"><a href=overView.php >&#127968</div>
+    </div>
+
 </body>
 </html>
