aacb04dd79dd0c7314870276d41340ad2c9696cd
Keanu M <113337515+KM-Vista@users.noreply.github.com>
2023-03-15 14:57:28 +0100

    newPost

---
+++ newPost.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="newProfile.css">
+</head>
+<body>
+    
+    <!--Home button-->
+    <div id="homeButton"><a href=index.php >&#127968 </a></div>
+
+    <!--greyCircle-->
+    <div id="greyCircle">
+        <br><br><br><br><br><br><br><br><br<br><br><br><br><br><br><br>
+        Foto toevoegen
+    </div>
+
+    <!--fashion description inputfield-->
+    <div id="fashionDescriptionInputField">
+    <label for="fashionDes">Fashion omschrijving:</label><br>
+    <input type="text" id="fashionDes" name="fashionDes">
+    </div>
+
+    <!--'save' button-->
+    <div id="saveButton">
+    <button type="button" onclick="alert('Je profiel is aangemaakt. Welkom!')">Opslaan</button>
+    </div>
+
+</body>
+</html>
